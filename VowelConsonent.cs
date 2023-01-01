646717efc1c0a9e8f1f12f9ead7e17b42e18074b
sumitradas94 <sumitradas94@gmail.com>
2023-01-02 01:11:28 +0530

    checking the Vowel or Consonent

---
+++ VowelConsonent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms
+{
+    public class VowelConsonent
+    {
+        public static void Vowel()
+        {
+            Console.WriteLine("\n***************check Ovel or Consonent****************\n ");
+            Console.WriteLine("enter single char from a to z: ");
+            char inputstring = Convert.ToChar(Console.ReadLine());
+
+
+            if (inputstring == 'a' || inputstring == 'e' ||
+             inputstring == 'i' || inputstring == 'o' ||
+             inputstring == 'u' || inputstring == 'A' ||
+             inputstring == 'E' || inputstring == 'I' ||
+             inputstring == 'O' || inputstring == 'U')
+            {
+                Console.WriteLine("vowel");
+            }
+            else
+            {
+                Console.WriteLine("consonent");
+            }
+        }
+    }
+}
+
