386f01d2144231829d284b5f2f90482c5d4ea741
sumitradas94 <sumitradas94@gmail.com>
2023-01-02 00:42:41 +0530

    Calculating Nth harmonic value

---
+++ HarmonicNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms
+{
+    public class HarmonicNumber
+    {
+        public static void nval()
+        {
+            Console.WriteLine("***************nth harmonic value***************");
+            Console.WriteLine("Hi, Please give the nth harmonic value");
+            int h = Convert.ToInt32(Console.ReadLine());
+            double result = 0.0;
+            if (h != 0)
+            {
+                for (int i = 1; i <= h; i++)
+                {
+
+                    result += 1 / (float)i;
+
+
+                }
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("enter a valid number");
+            }
+        }
+    }
+}
