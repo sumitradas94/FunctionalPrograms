fadb4d6d7cb23ecfa8ae36713de7ba15873ffa15
sumitradas94 <sumitradas94@gmail.com>
2023-01-02 01:18:34 +0530

    This is leapyear program

---
+++ Leapyear.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms
+{
+    public class Leapyear
+    {
+        public static void leap()
+        {
+            Console.WriteLine("Enter the year and check the year is leaf year or not");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if ((year > 999) && (year < 10000))
+            {
+                if (year % 400 == 0)
+                {
+                    Console.WriteLine("This Year is leap year");
+                }
+                else if ((year % 4 == 0) && (year % 100 != 0))
+                {
+                    Console.WriteLine("This year is leap year");
+                }
+                else
+                {
+                    Console.WriteLine("This year is not leap year");
+                }
+            }
+            else
+            {
+                Console.WriteLine("enter a valid year");
+
+            }
+        }
+    }
+}
