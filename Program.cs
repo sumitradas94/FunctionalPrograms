b84b582d7cb2711d1c3d856674ef07c458022f26
sumitradas94 <sumitradas94@gmail.com>
2023-01-02 00:38:06 +0530

    Greeting

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using FunctionalPrograms;
+
+namespace Assignment1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+           // leap.display();
+
+            //PowerN.pow();
+
+           // HarmonicNumber.nval();
+
+            //pFactorial.display1();
+
+            //flipCoin.flips();
+
+           // QueRem.Qre();
+
+           // Swap.swaping();
+
+            EvenOdd.check();
+
+           // VowelConsonent.checkk();
+
+           // LargestNumber.large();
+        }
+    }
+}
