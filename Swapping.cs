8f565162c8d20a986e356b1c6312d0e8d21f69e9
sumitradas94 <sumitradas94@gmail.com>
2023-01-02 01:06:26 +0530

    Swapping two number

---
+++ Swapping.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms
+{
+    public class Swapping
+    {
+        public static void swaping()
+        {
+            int temp = 0;
+            int b = 5;
+            Console.WriteLine("***************swaping the values****************\n ");
+            Console.WriteLine("enter the 'a' value: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("the a value is : " + a);
+            Console.WriteLine("the b value is : " + b);
+
+
+            if (a > 0)
+            {
+                temp = a;
+                a = b;
+                b = temp;
+            }
+            Console.WriteLine("the a value is : " + a);
+            Console.WriteLine("the b value : " + b);
+        }
+    }
+}
