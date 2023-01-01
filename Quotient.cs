249a8f7dea27990c0c944a8f175ceff0355a9b25
sumitradas94 <sumitradas94@gmail.com>
2023-01-02 01:03:32 +0530

    Quotient and Remainder of a number calculate

---
+++ Quotient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms
+{
+    public class Quotient
+    {
+        public static void Remainder()
+        {
+            double q, r;
+
+            Console.WriteLine("***************Quotient and Remainder***************");
+            Console.WriteLine("\nPlease Enter a number to Find Quotient and Remainder :  ");
+
+            double n = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nEnter a number to know on which number you are operating to find your Quotient and Remainder :  ");
+
+            double m = Convert.ToDouble(Console.ReadLine());
+
+            r = n % m;
+            q = n / m;
+
+            Console.WriteLine("Your Quotient is  :: {0} \nYour Remainder is :: {1} ", q, r);
+        }
+    }
+}
